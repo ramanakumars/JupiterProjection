ab3fdf083f1cfabe65f8a4777253953ccdb5c2c4
Ramanakumar Sankar <ramanakumar.shankar@gmail.com>
2020-08-21 20:35:46 -0400

    Moved the function calls to a new script

--- projection_funcs.py
+++ projection_funcs.py
@@ -7,11 +7,10 @@ import spiceypy, glob
 
 KERNEL_DATAFOLDER = '/home/local/Isis/data/juno/kernels/'
 pcks  = sorted(glob.glob(KERNEL_DATAFOLDER+"pck/pck*.tpc"))
-spks2 = sorted(glob.glob(KERNEL_DATAFOLDER+"spk/jup*.bsp"))
-#spks3 = sorted(glob.glob(KERNEL_DATAFOLDER+"spk/de438.bsp"))
+spks  = sorted(glob.glob(KERNEL_DATAFOLDER+"spk/jup*.bsp"))
 lsks  = sorted(glob.glob(KERNEL_DATAFOLDER+"lsk/naif*.tls"))
 
-kernels = [pcks[-1], spks2[-1], lsks[-1]]
+kernels = [pcks[-1], spks[-1], lsks[-1]]
 for kernel in kernels:
     spiceypy.furnsh(kernel)
 
@@ -176,7 +175,7 @@ def map_project(vecs, et):
     print()
     return (lon, lat)
 
-def plot_map(lon, lat, img, pixres=0.05):
+def plot_map(lon, lat, img, pixres=0.1):
     '''
         project the image onto a lat/lon grid
     '''
@@ -212,31 +211,3 @@ def plot_map(lon, lat, img, pixres=0.05):
 
     plt.imsave("map.png", IMG, origin='lower')
 
-et  = spiceypy.utc2et('2019-02-02 10:07:06')
-img = plt.imread('2019-02-02-1007_1-RGBdp.jpg')/255.
-'''
-vecs     = get_vec_from_image(img, et)
-
-lon, lat = map_project(vecs, et)
-
-fig = plt.figure(figsize=(20,8))
-ax1 = fig.add_subplot(131)
-ax2 = fig.add_subplot(132)
-ax3 = fig.add_subplot(133)
-
-ax1.imshow(lon, vmin=0., vmax=360.)
-ax2.imshow(lat, vmin=-90., vmax=90.)
-ax3.imshow(img)
-
-plt.tight_layout()
-
-fig.savefig('projected.png', dpi=150)
-
-np.save('lon.npy', lon, allow_pickle=False)
-np.save('lat.npy', lat, allow_pickle=False)
-'''
-
-lat = np.load('lat.npy')
-lon = np.load('lon.npy')
-
-plot_map(lon, lat, img)
